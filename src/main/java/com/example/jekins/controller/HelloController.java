33685b8218c1ef59bf0fbc9b1dfd961be184694d
wangxu <593464677@qq.com>
2019-05-02 23:17:54 +0800

    新增/

--- src/main/java/com/example/jekins/controller/HelloController.java
+++ src/main/java/com/example/jekins/controller/HelloController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @GetMapping("/")
+    public String test(){
+        return "请求接口为/";
+    }
+
     @GetMapping("/index")
     public String index(){
         return "Welcome To Index Page !";
