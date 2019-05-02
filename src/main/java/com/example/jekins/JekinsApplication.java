7de1c2c059bfa6ede473207c12e872530db5cb12
wangxu <46063482+15583000287@users.noreply.github.com>
2019-05-02 11:37:35 +0800

    Update JekinsApplication.java

--- src/main/java/com/example/jekins/JekinsApplication.java
+++ src/main/java/com/example/jekins/JekinsApplication.java
@@ -13,8 +13,8 @@ public class JekinsApplication {
         SpringApplication.run(JekinsApplication.class, args);
     }
 
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/hello")
+    public String hello(){
         return "Hello Welcome to Jenkins!  这是我使用Jenkins + Docker自动化部署的第一个应用！";
     }
 }
