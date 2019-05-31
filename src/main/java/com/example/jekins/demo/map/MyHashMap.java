8d7b2f502ce1bc2d6d0015fb6796c6214791ca1d
wangxu <593xxx677@qq.com>
2019-06-01 00:19:07 +0800

    手写HashMap

--- src/main/java/com/example/jekins/demo/map/MyHashMap.java
+++ src/main/java/com/example/jekins/demo/map/MyHashMap.java
@@ -2,32 +2,47 @@ package com.example.jekins.demo.map;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 手写HashMap  参考博客：https://blog.csdn.net/huangshulang1234/article/details/79713303
+ *
  * @param <K>
  * @param <V>
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
     /**
-     * 初始容量 aka 16   00000001 --> 00010000
+     * 默认初始容量 aka 16   00000001 --> 00010000
      */
     static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
     /**
      * 默认加载因子（构造函数为指定时使用）
      */
     static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    /**
+     * 初始容量
+     */
     private int defaultInitialCapacity;
+    /**
+     * 加载因子
+     */
     private float defaultLoadFactor;
+
     /**
      * Map中Entry数量
      */
     private int size;
 
     /**
-     * 数组
+     * 数组(核心)
      */
-    private Entry<K, V>[] table = null;
+    private Entry<K, V>[] table;
+
+    /**
+     * TODO key的Set集合
+     */
+    transient Set<MyMap.Entry<K, V>> entrySet;
 
     /**
      * 构造方法(“门面模式” 这里的2个构造方法其实指向的是同一个，但是对外却暴露了2个“门面”！)
@@ -45,6 +60,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
         this.defaultInitialCapacity = defaultInitialCapacity;
         this.defaultLoadFactor = defaultLoadFactor;
+
+        //初始Entry数组
+        this.table = new Entry[defaultInitialCapacity];
     }
 
 
@@ -74,53 +92,102 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         public V getValue() {
             return value;
         }
-    }
 
+        @Override
+        public String toString() {
+            return key + "=" + value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            //TODO
+            return super.equals(obj);
+        }
+    }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
+    /**
+     * 思路：1. 根据Key生成Hash值(Entry数组的索引，关键)
+     * 存在：比较新增key与该元素的key（==）；若不等，继续比较新增key与原来的key（equals方法）
+     * 相等：覆盖原来的value,返回原来的value
+     * 不相等(hash值相同)：新建Entry,将原索引处的Entry放入新Entry的next中，形成链表
+     *
+     * @param k
+     * @param v
+     * @return
+     */
     @Override
     public V put(K k, V v) {
         V oldValue = null;
-        //判断是否需要扩容，若扩容肯定需重新散列
+        //判断是否需要扩容，若扩容肯定需重新散列 resize()+rehash()
         if (size >= defaultInitialCapacity * defaultLoadFactor) {
             resize(2 * defaultInitialCapacity);
         }
-        //TODO 得到Hash值，计算出数组中的位置
+        //TODO 得到Hash值，计算出数组中的位置(关键，决定是否产生链表)
         int index = hash(k) & (defaultInitialCapacity - 1);
+        //情况一： Entry数组中，索引index处为空,则赋值 (根据hash值判断此索引是否有元素，每次存放的位置取决于hash值)
         if (table[index] == null) {
             table[index] = new Entry<K, V>(k, v, null);
             ++size;
-        } else {//需要遍历单链表
+        } else {//情况二：若存在，需要遍历单链表
             Entry<K, V> entry = table[index];
             Entry<K, V> e = entry;
             while (e != null) {
+                //情况1：先比较key地址之是否相等，若不等则equals方法比较key
                 if (k == e.getKey() || k.equals(e.getKey())) {
                     oldValue = e.value;
+                    //key相等，当value则覆盖原来的value，并返覆盖的元素
                     e.value = v;
                     return oldValue;
                 }
+                //获取单链表下一个Entry,赋给临时遍历e,继续遍历
                 e = e.next;
             }
+            //情况2：key不相等，hash值相等 ,此索引位置放新建的Entry,并将原索引的Entry放入next中形成链表
             table[index] = new Entry<K, V>(k, v, entry);
-            ++ size;
+            ++size;
         }
         return oldValue;
     }
 
     @Override
     public V get(K k) {
+        //根据key计算hash值
+        int index = hash(k) & (defaultInitialCapacity - 1);
+        if (table[index] == null) {
+            return null;
+        } else {//如果不为空，注意遍历链表
+            Entry<K, V> entry = table[index];
+            do {
+                //判断key是否相等，相等返回值(do...while循环至少保证执行一次)
+                if (k == entry.getKey() || k.equals(entry.getKey())) {
+                    return entry.value;
+                }
+                //将下一个entry赋给临时变量entry
+                entry = entry.next;
+            } while (entry != null);
+        }
         return null;
     }
 
+    //TODO 获取key的Set集合
+    @Override
+    public Set<MyMap.Entry<K, V>> entrySet() {
+        return null;
+    }
+
+    /**
+     * 重构Entry数组
+     */
     private void resize(int i) {
         Entry[] newTable = new Entry[i];
         defaultInitialCapacity = i;
@@ -128,6 +195,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         rehash(newTable);
     }
 
+    /**
+     * 重构hash(将所有元素重新put)
+     */
     private void rehash(Entry<K, V>[] newTable) {
         //得到原来老的Entry集合,注意遍历单链表
         List<Entry<K, V>> entryList = new ArrayList<>();
@@ -155,9 +225,23 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     /**
      * Hash函数
      */
-    private int hash(K k) {
-        int hashCode = k.hashCode();
-        hashCode = (hashCode >>> 20) ^ (hashCode >>> 12);
-        return hashCode ^ (hashCode >>> 7) ^ (hashCode >>> 4);
+    final int hash(K k) {
+//        int hashCode = k.hashCode();
+//        hashCode = (hashCode >>> 20) ^ (hashCode >>> 12);
+//        return hashCode ^ (hashCode >>> 7) ^ (hashCode >>> 4);  //注释的算法导致，每次得到的索引的都是0，索引相同导致产生链表
+        int h;
+        return (k == null) ? 0 : (h = k.hashCode()) ^ (h >>> 16);
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer("[");
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null) {
+                sb.append(table[i].toString() + ", ");
+            }
+        }
+        sb.replace(sb.length() - 2, sb.length() - 1, "]");
+        return sb.toString();
     }
 }
--- src/main/java/com/example/jekins/demo/test/MyHashMapTest.java
+++ src/main/java/com/example/jekins/demo/test/MyHashMapTest.java
@@ -7,9 +7,16 @@ import com.example.jekins.demo.map.MyMap;
  * MyHashMap测试
  */
 public class MyHashMapTest {
-    public static void main(String[] args){
-        MyMap<String,String> map = new MyHashMap();
-        map.put("p1","张三");
-        System.out.println(map.size());
+    public static void main(String[] args) {
+        MyMap<String, String> map = new MyHashMap();
+        System.out.println("------------------测试put(K k,V v)-----------------------");
+        map.put("p1", "张三");
+        map.put("p2", "李四");
+        map.put("p3", "王五");
+        System.out.println("size: " + map.size());
+
+        System.out.println("------------------测试get(K k)-----------------------");
+        System.out.println("p1: " + map.get("p1"));
+        System.out.println(map);
     }
 }
